b34bd1b5c25c2bc6a240dc966af682458ad49a52
Makcim Motruk <56037758+MakcFaktor@users.noreply.github.com>
2024-08-28 19:40:01 +0300

    edit

--- src/api.js
+++ src/api.js
@@ -2,5 +2,22 @@ import axios from 'axios';
 
 const API_URL = process.env.REACT_APP_API_URL;
 
-export const getHotels = () => axios.get(`${API_URL}/hotels`);
-export const submitBooking = (data) => axios.post(`${API_URL}/bookings`, data);
+export const getHotels = async () => {
+    try {
+        const response = await axios.get(`${API_URL}/hotels`);
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching hotels:", error);
+        throw error;
+    }
+};
+
+export const submitBooking = async (data) => {
+    try {
+        const response = await axios.post(`${API_URL}/bookings`, data);
+        return response.data;
+    } catch (error) {
+        console.error("Error submitting booking:", error);
+        throw error;
+    }
+};
--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const About = () => (
-    <div>
+    <div  style={{ margin: '50px' }}>
         <h1>About Us</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aut autem blanditiis consectetur dolore doloribus eaque esse ex explicabo facilis fugit, iusto, laboriosam laudantium mollitia nisi non possimus quae quaerat quam quas quia sequi sunt voluptatum? Autem eos eveniet itaque porro veritatis voluptas voluptatum. Ab aperiam dolor ea iusto labore neque provident quae! Aliquam distinctio ea eos esse illum magni maxime necessitatibus, nesciunt quaerat ullam! Architecto blanditiis, culpa cumque enim eum explicabo facilis impedit inventore modi molestias nulla odit officia possimus quas repudiandae sint veniam! Dicta dolor eligendi, eos id incidunt mollitia nisi numquam, placeat quisquam sunt totam ullam vel?</p>
     </div>
